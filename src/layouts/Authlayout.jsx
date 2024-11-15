3e89f384b976f296beb79c9e57a0df64441d053a
tanver909 <tanveermahidi@gmial.com>
2024-11-15 19:21:36 +0600

    project sarted for authentication

---
+++ src/layouts/Authlayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Authlayout = () => {
+  return (
+    <div>
+  
+      Authlayout
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Authlayout;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
 import CategoryNews from "../pages/CategoryNews";
+import Authlayout from "../layouts/Authlayout";
 
 const router = createBrowserRouter([
   {
@@ -27,7 +28,17 @@ const router = createBrowserRouter([
   },
   {
     path: "auth",
-    element: <h1>Login</h1>,
+    element: <Authlayout></Authlayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <h2>login</h2>
+      },
+      {
+        path: "/auth/register",
+        element: <h2>register</h2>
+      }
+    ]
   },
   {
     path: "*",
